4d2996056a8eab429f7d16270fb5b2a683ab48c6
dantee1997 <37018947+dantee1997@users.noreply.github.com>
2019-04-10 02:47:30 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,18 @@
+var byline = document.getElementById('byline');     // Find the H2
+bylineText = byline.innerHTML;                                      // Get the content of the H2
+bylineArr = bylineText.split('');                                   // Split content into array
+byline.innerHTML = '';                                                      // Empty current content
+
+var span;                   // Create variables to create elements
+var letter;
+
+for(i=0;i<bylineArr.length;i++){                                    // Loop for every letter
+  span = document.createElement("span");                    // Create a <span> element
+  letter = document.createTextNode(bylineArr[i]);   // Create the letter
+  if(bylineArr[i] == ' ') {                                             // If the letter is a space...
+    byline.appendChild(letter);                 // ...Add the space without a span
+  } else {
+        span.appendChild(letter);                       // Add the letter to the span
+    byline.appendChild(span);                   // Add the span to the h2
+  }
+}
